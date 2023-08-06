ad2795a17e31f234b33a69062591004bd3ed57d4
litansarkar95 <litansarkar95@gmail.com>
2023-08-06 21:17:42 +0600

    serializer GET, POST

--- watchmate/watchlist_app/api/serializers.py
+++ watchmate/watchlist_app/api/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
+from watchlist_app.models import Movie
 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
     description = serializers.CharField()
     active = serializers.BooleanField()
+
+    def create(self,validation_data):
+        return Movie.objects.create(**validation_data)
--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -4,12 +4,21 @@ from watchlist_app.api.serializers import MovieSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-@api_view()
+@api_view(['GET', 'POST'])
 def movie_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
+    if request.method == 'GET':
 
-    return Response(serializer.data)
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+    if request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
 
 @api_view()
 def movie_details(request,pk):
