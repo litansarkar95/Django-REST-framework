da8aa5b3f111e470a0df6e2bb1b02474115891a1
litansarkar95 <litansarkar95@gmail.com>
2023-08-07 21:29:53 +0600

    DELETE

--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -1,6 +1,7 @@
 from watchlist_app.models import Movie
 from watchlist_app.api.serializers import MovieSerializer
 
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -24,9 +25,14 @@ def movie_list(request):
 def movie_details(request,pk):
     if request.method == 'GET':
      
-     movie = Movie.objects.get(pk=pk)
-     serializer = MovieSerializer(movie)
-     return Response(serializer.data)
+        try:
+            movie = Movie.objects.get(pk=pk)
+
+        except Movie.DoesNotExist:
+           return Response({'error': "Movie Not found"},status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
     
     if request.method == 'PUT':
         movie = Movie.objects.get(pk=pk)
@@ -35,7 +41,12 @@ def movie_details(request,pk):
             serializer.save()
             return Response(serializer.data)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    if request.method == 'DELETE':
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
     
     
 
