a09ceadd9b26cbdea93d3f201a0baac873331956
litansarkar95 <litansarkar95@gmail.com>
2023-08-09 21:21:01 +0600

    Class Base

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path 
-from watchlist_app.api.views import movie_list ,movie_details
+#from watchlist_app.api.views import movie_list ,movie_details
+from watchlist_app.api.views import MovieListAV, MovieDetailsAV
 
 urlpatterns = [
-    path('list/',movie_list, name='movie-list'),
-    path("<str:pk>/",movie_details,name="movie-details")
+    path('list/',MovieListAV.as_view(), name='movie-list'),
+    path("<str:pk>/",MovieDetailsAV.as_view(),name="movie-details")
   
 ]
--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -3,7 +3,46 @@ from watchlist_app.api.serializers import MovieSerializer
 
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 # from rest_framework.decorators import api_view
+
+class MovieListAV(APIView):
+    def get(self,request):
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+    
+    def post(self,request):
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+        
+
+class MovieDetailsAV(APIView):
+    def get(self,request,pk):
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+    
+    def put(self,request,pk):
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist():
+             return Response({'error': "Movie Not found"},status=status.HTTP_404_NOT_FOUND)
+            
+        
+        serializer = MovieSerializer(movie)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+
+
 
 #@api_view(['GET', 'POST'])
 # def movie_list(request):
