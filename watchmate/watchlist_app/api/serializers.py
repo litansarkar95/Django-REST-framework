8f2ef4938756f1232481ea0b919c7518ddf930e7
litansarkar95 <litansarkar95@gmail.com>
2023-08-11 22:29:36 +0600

    validation error

--- watchmate/watchlist_app/api/serializers.py
+++ watchmate/watchlist_app/api/serializers.py
@@ -16,3 +16,10 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validation_data.get('active',instance.active)
         instance.save()
         return instance
+    
+    def validate_name(self, value):
+         
+         if len(value) < 2:
+             raise serializers.ValidationError("Name is too short!")
+         else:
+             return value
