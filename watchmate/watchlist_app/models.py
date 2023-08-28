594f260f77bccee49f54d4b05686b74fb584e9e0
litansarkar95 <litansarkar95@gmail.com>
2023-08-28 22:18:13 +0600

    models

--- watchmate/watchlist_app/models.py
+++ watchmate/watchlist_app/models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 
 # Create your models here.
-class Watch(models.Model):
-    title = models.CharField(max_length=50)
+class StreamPlatform(models.Model):
+    name = models.CharField(max_length=30)
+    about = models.CharField(max_length=150)
+    website = models.URLField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
     
 class WatchList(models.Model):
